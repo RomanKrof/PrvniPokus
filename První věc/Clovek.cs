861e8f68f180c53a008f9dd92cf7150363b86979
Roman Krof <krof.roman.2018@skola.ssps.cz>
2021-09-07 15:17:55 +0200

    upravit tostring

---
+++ První věc/Clovek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace První_věc
+{
+    class Clovek
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+
+        public Clovek()
+        {
+            Jmeno = "Nezname";
+            Prijmeni = "Nezname";
+        }
+
+        public Clovek(string Jmenoo, string Prijmenii)
+        {
+            Jmeno = Jmenoo;
+            Prijmeni = Prijmenii;
+        }
+
+        public override string ToString()
+        {
+            return $"Jmeno; {Jmeno}" +
+                $"Prijmeni: {Prijmeni}";
+        }
+    }
+}
---
+++ První věc/Obcan.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace První_věc
+{
+    class Obcan : Clovek
+    {
+        Regex rx = new Regex(@"\d{6}\/\d{4}");
+
+        string RodneCislo
+        {
+            get { return RodneCislo; }
+            set
+            {
+                if (rx.IsMatch(value))
+                {
+                    RodneCislo = value;
+                }
+                else
+                {
+                    Exception except = new FormatException("Toto není rodné číslo!");
+                    throw except;
+                }
+            }
+        }
+
+        public Obcan()
+        {            
+            Jmeno = "Nezname";
+            Prijmeni = "Nezname";
+            RodneCislo = "111111/1111";
+        }
+
+        public Obcan(Clovek clovek, string RodneCisloo)
+        {
+            Jmeno = clovek.Jmeno;
+            Prijmeni = clovek.Prijmeni;
+            RodneCislo = RodneCisloo;
+        }
+
+        public override string ToString()
+        {
+            return $"Jmeno; {Jmeno}" +
+                $"Prijmeni: {Prijmeni}" +
+                $"Rodne cislo: {RodneCislo}";
+        }
+    }
+}
