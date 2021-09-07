d31e9e000394a765b6d72b663d09bff0b306673b
Krof Roman <krof.roman.2018@skola.ssps.cz>
2021-09-07 13:57:00 +0200

    Přidejte soubory projektu.

---
+++ První věc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace První_věc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
